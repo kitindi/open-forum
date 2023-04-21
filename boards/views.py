51ae44979336f613b5617ae8a79242284c446a6f
Abdulaziz Sadi <abdullkitindi@gmail.com>
2023-04-21 22:30:25 +0300

    created super user and added Board model to the django admin pannel

--- boards/views.py
+++ boards/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import Board
 
 # Create your views here.
 
 
 def index(request, *args, **kwargs):
-    return render(request,'index.html')
+    
+    return render(request,'index.html' )
 
 
 def home(request, *args, **kwargs):
-    return render(request, 'home.html')
+    boards = Board.objects.all()
+    return render(request, 'home.html',{'boards': boards})
