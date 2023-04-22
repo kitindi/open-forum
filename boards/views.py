77887c541dd4c43cfdb5224c495a68b9ea9c968f
Abdulaziz Sadi <abdullkitindi@gmail.com>
2023-04-22 10:47:22 +0300

    added board page and topic related page layout

--- boards/views.py
+++ boards/views.py
@@ -12,3 +12,7 @@ def index(request, *args, **kwargs):
 def home(request, *args, **kwargs):
     boards = Board.objects.all()
     return render(request, 'home.html',{'boards': boards})
+
+def board_topics(request,pk):
+    board = Board.objects.get(id=pk)
+    return render(request, 'board_topics.html', {'board':board})
